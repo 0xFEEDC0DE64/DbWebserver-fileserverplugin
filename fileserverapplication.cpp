bdb57f145a1d91fbe4d5de53c23e06f61b57e32b
0xFEEDC0DE64 <0xFEEDC0DE64@gmail.com>
2018-09-22 03:07:20 +0200

    Implemented basic example response

--- fileserverapplication.cpp
+++ fileserverapplication.cpp
@@ -1,5 +1,9 @@
 #include "fileserverapplication.h"
 
+#include "httprequest.h"
+#include "httpresponse.h"
+#include "httpclientconnection.h"
+
 FileserverApplication::FileserverApplication(const QJsonObject &config, QObject *parent) :
     WebApplication(parent)
 {
@@ -9,4 +13,12 @@ FileserverApplication::FileserverApplication(const QJsonObject &config, QObject 
 void FileserverApplication::start()
 {
 
+}
+
+void FileserverApplication::handleRequest(HttpClientConnection *connection, const HttpRequest &request)
+{
+    HttpResponse response;
+    response.protocol = request.protocol;
+    response.statusCode = HttpResponse::StatusCode::OK;
+    connection->sendResponse(response, "Hello from FileserverApplication: " + request.path);
 }
